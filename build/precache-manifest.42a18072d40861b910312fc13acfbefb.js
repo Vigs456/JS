a00ae0c213ea5a9c724bda827e9f6e6416d294e2
Vigs456 <vigs456@gmail.com>
2020-02-20 02:13:40 -0800

    Build

---
+++ build/precache-manifest.42a18072d40861b910312fc13acfbefb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9fdba4b730ef7346a923daf1a2fdd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2b605478d403547861",
+    "url": "/static/css/3.f235769c.chunk.css"
+  },
+  {
+    "revision": "b7e19ddf81b4e2559167",
+    "url": "/static/css/4.64ab5563.chunk.css"
+  },
+  {
+    "revision": "7bf55fc2cd620331a032",
+    "url": "/static/css/main.3f43a1fe.chunk.css"
+  },
+  {
+    "revision": "920bd0f1f3e04c870d7d",
+    "url": "/static/js/2.00e284a2.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/static/js/2.00e284a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f2b605478d403547861",
+    "url": "/static/js/3.7744b8fb.chunk.js"
+  },
+  {
+    "revision": "b7e19ddf81b4e2559167",
+    "url": "/static/js/4.5841a94a.chunk.js"
+  },
+  {
+    "revision": "7bf55fc2cd620331a032",
+    "url": "/static/js/main.3f0ed3dd.chunk.js"
+  },
+  {
+    "revision": "649e5c5d6ba36d7b20cc",
+    "url": "/static/js/runtime-main.d6695f43.js"
+  },
+  {
+    "revision": "98e496709bbef4f671c18125db007cf5",
+    "url": "/static/media/preloader.98e49670.gif"
+  },
+  {
+    "revision": "ce8e7ab80c30d435d1557087b2a130dc",
+    "url": "/static/media/user.ce8e7ab8.png"
+  }
+]);
